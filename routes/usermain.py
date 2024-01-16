264538cf5ee4e4a9e093938b1587f6f24bfeb3a6
sky <adsky0309@gmail.com>
2024-01-16 11:38:39 +0900

    d

--- routes/usermain.py
+++ routes/usermain.py
@@ -122,9 +122,17 @@ async def exchange(request:Request, gifty_style):
 @router.get("/exchange/detail/{object_id}") # 펑션 호출 방식
 async def exchange(request:Request, object_id):
     condition = {'_id' : {'$regex' : object_id}}
-    gifty_list = await collection_gifty.getsbyconditions(condition)
+    gifty = await collection_gifty.getsbyconditions(condition)
     return templates.TemplateResponse(name="exchange/gifticon_detail.html", context={'request':request,
-                                                                                     "gifty_list": gifty_list})
+                                                                                     "gifty": gifty})
+
+# 쿠폰교환페이지에서 쿠폰구매를 눌렀을 때 : 주소 /
+@router.get("/exchange/order/{user_id}/{object_id}")  # 구매하는 사용자의 ID 필요
+async def order(request : Request, user_id, object_id ) :
+    return templates.TemplateResponse(name="exchange/gifticon_order.html", context= {'request':request,})
+
+
+# 쿠폰교환페이지에서 쿠폰구매를 눌렀을 때(로그인이 되지 않았을 경우 로그인 페이지로 이동) : 주소 /
 
 # 공지사항 클릭했을 때 : 주소 /clicktech/notice
 @router.get("/notice") # 펑션 호출 방식
